6d37fc17ff26d501cb9a5700851e86068ff7490b
Sarah Francis <sarah.katherinefrancis@gmail.com>
2023-03-13 16:55:01 +0200

    chore: added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+
+  resources :users, only: [:show]
+
+  resources :trips, only: [:index, :new, :create] do
+    resources :activities, only: [:index, :create, :update]
+  end
+
+  resources :activities, only: [:index] do
+    resources :notes, only: [:create]
+  end
 end
