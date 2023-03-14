79dacdb23622e4028c63e4533bb84c2889864459
Kirsty Brown <kirstyannbrown4@gmail.com>
2023-03-14 13:33:00 +0200

    chore:created users controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2023_03_14_102431) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_14_110151) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
