3b7bc50ed9ae680f0e39b97e78bf4de778e71547
Oscar Welton <oscarwelton@gmail.com>
2023-03-14 12:55:18 +0200

    added a new trip view path

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -5,6 +5,19 @@ class TripsController < ApplicationController
     @user = current_user
   end
 
+  def new
+    @trip = Trip.new
+  end
 
+  def create
+    @trip = Trip.new(trip_params)
+    @trip.user = current_user
+    redirect_to trip_path(@trip) if @trip.save
+  end
 
+  private
+
+  def trip_params
+    params.require(:trip).permit(:start_date, :end_date, :trip_name, :user_id)
+  end
 end
