a1aefc7456773f8eaeee22bd3da726b9b622a78b
Sarah Francis <sarah.katherinefrancis@gmail.com>
2023-03-14 19:49:21 +0200

    seed: created user, trip and activities seeds

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,7 +2,7 @@ class Activity < ApplicationRecord
   belongs_to :trip
   has_many :notes, dependent: :destroy
 
-  enum type: {
+  enum category: {
     eat: 0,
     do: 1,
     explore: 2
--- db/seeds.rb
+++ db/seeds.rb
@@ -45,14 +45,100 @@ puts "created trip for user with id:#{trip.user.id}"
 
 puts "creating activities suggestions for the trip"
 
-activity1 = Activity.create!(
+eat_activity1 = Activity.create!(
   name: 'Plant Cafe',
   description: 'This vegan cafe in Cape Town serves a range of delicious plant-based meals, including burgers, salads,
    and smoothies. They also have outdoor seating that is dog-friendly, so you can bring your furry friend along while
     you enjoy your meal.',
-  type: 0,
+  category: 0,
   status: 0,
   trip: trip
 )
 
-puts "created activity with the type: #{activity1.type}"
+eat_activity2 = Activity.create!(
+  name: 'Raw and Roxy',
+  description: 'Raw and Roxy is a vegan restaurant that specializes in raw, organic, and gluten-free cuisine. They have
+  a range of delicious dishes, including smoothie bowls, salads, and vegan sushi. They also have outdoor seating that
+  is dog-friendly.',
+  category: 0,
+  status: 0,
+  trip: trip
+)
+
+eat_activity3 = Activity.create!(
+  name: "Scheckter's Raw",
+  description: "Scheckter's Raw is a vegan and organic restaurant that serves a range of delicious plant-based dishes,
+  including burgers, salads, and smoothies. They also have outdoor seating that is dog-friendly, so you can enjoy your
+  meal with your furry friend by your side.",
+  category: 0,
+  status: 0,
+  trip: trip
+)
+
+do_activity1 = Activity.create!(
+  name: "Hike up Table Mountain",
+  description: "The hike to the top can be challenging but rewarding, with several trails to choose from depending on
+  your fitness level and experience. Once at the top, take in the stunning views, have a picnic, and explore the many
+  rock formations.",
+  category: 1,
+  status: 0,
+  trip: trip
+)
+
+do_activity2 = Activity.create!(
+  name: "Swim at Silvermine Nature Reserve",
+  description: "Silvermine Nature Reserve is a beautiful conservation area located in the mountains above Cape Town.
+  The reserve offers several hiking trails with stunning views, as well as a large reservoir that's perfect for swimming
+   and picnicking.",
+  category: 1,
+  status: 0,
+  trip: trip
+)
+
+do_activity3 = Activity.create!(
+  name: "Attend a coding workshop at Le Wagon Cape Town",
+  description: "Le Wagon is a world-renowned coding bootcamp that offers immersive courses in web development, data
+  science, and more.",
+  category: 1,
+  status: 0,
+  trip: trip
+)
+
+explore_activity1 = Activity.create!(
+  name: "Table Mountain",
+  description: "This iconic landmark is a must-visit attraction in Cape Town. You can hike up to the top or take the
+  cable car and enjoy stunning panoramic views of the city, ocean, and surrounding landscape.",
+  category: 2,
+  status: 0,
+  trip: trip
+)
+
+explore_activity2 = Activity.create!(
+  name: "Robben Island",
+  description: "A UNESCO World Heritage Site and a former political prison where Nelson Mandela was held for 18 years.
+  You can take a ferry from the Victoria & Alfred Waterfront to explore the island and learn about South Africa's
+  history.",
+  category: 2,
+  status: 0,
+  trip: trip
+)
+
+explore_activity3 = Activity.create!(
+  name: "Cape Peninsula",
+  description: "A scenic drive along the Cape Peninsula offers breathtaking views of the Atlantic Ocean, mountains, and
+  beaches. Don't miss the chance to visit Boulders Beach to see the African penguins, and Cape Point Nature Reserve to
+  see the Cape of Good Hope.",
+  category: 2,
+  status: 0,
+  trip: trip
+)
+
+puts "created activity with the type: #{eat_activity1.category}"
+puts "created activity with the type: #{eat_activity2.category}"
+puts "created activity with the type: #{eat_activity3.category}"
+puts "created activity with the type: #{do_activity1.category}"
+puts "created activity with the type: #{do_activity2.category}"
+puts "created activity with the type: #{do_activity3.category}"
+puts "created activity with the type: #{explore_activity1.category}"
+puts "created activity with the type: #{explore_activity2.category}"
+puts "created activity with the type: #{explore_activity3.category}"
