d67f99cd0bb0853358b62bd6ab255bcdc556eef5
Sarah Francis <sarah.katherinefrancis@gmail.com>
2023-03-14 21:15:11 +0200

    clean: cleaned up the migrations

--- db/migrate/20230313195247_change_status_type_in_activities.rb
+++
@@ -1,5 +0,0 @@
-class ChangeStatusTypeInActivities < ActiveRecord::Migration[7.0]
-  def change
-    change_column :activities, :status, :integer
-  end
-end
--- db/migrate/20230314110151_add_preferences_to_user.rb
+++
@@ -1,6 +0,0 @@
-class AddPreferencesToUser < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :eat_preferences, :string
-    add_column :users, :do_preferences, :string
-  end
-end
