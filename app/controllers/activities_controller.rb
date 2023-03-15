c5dfa79e0f5de175cc7ed44c9c8c5f7aa63f8f10
Oscar Welton <oscarwelton@gmail.com>
2023-03-15 11:54:45 +0200

    chore: activities sorted and stored based on category

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,24 @@
 class ActivitiesController < ApplicationController
   def index
-    @activities = Activity.all
+    sorted_activities
   end
+
+  def sorted_activities
+    @activities = Activity.all
+    @eat = []
+    @do = []
+    @explore = []
+
+    @activities.each do |activity|
+      case activity.category
+      when "eat"
+        @eat << activity
+      when "do"
+        @do << activity
+      when "explore"
+        @explore << activity
+      end
+    end
+  end
+
 end
