ec0a220eec5d5175832bc25537705f46bfa970ee
Sarah Francis <sarah.katherinefrancis@gmail.com>
2023-03-16 11:49:48 +0200

    feature: when user creates a new trip they can see suggestions based on preferences

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -7,36 +7,45 @@ class ActivitiesController < ApplicationController
     do_preference = @user.do_preference_list
 
     client = OpenAI::Client.new
-    eat_prompt = "I am traveling to #{destination}. Can you suggest 3 restaurants that are #{eat_preference}? Response must be in a ruby hash with the keys name:, description:,  category: 0 and status: 0"
-    do_prompt = "I am traveling to #{destination}. Can you suggest 3 activities that involve #{do_preference}? Response
-    must include name and description"
-    explore_prompt = "I am traveling to #{destination}. Can you suggest the top 3 things to explore in the area?
-    Response must include name and description"
+    eat_prompt = "Can you suggest 3 restaurants that are #{eat_preference} in #{destination}?
+    Response must be in a JSON. JSON has an array with objects that has keys called name and description."
+    do_prompt = "Can you suggest 3 activities that are #{do_preference} in #{destination}?
+    Response must be in a JSON. JSON has an array with objects that has keys called name and description."
+    explore_prompt = "Can you suggest 3 activities top in #{destination}? Response must be in a JSON. JSON has an array
+    with objects that has keys called name and description."
 
     eat_response = client.completions(
       parameters: {
-        model: 'gpt-4',
+        model: 'text-davinci-003',
         prompt: eat_prompt,
-        max_tokens: 600
+        temperature: 0.3,
+        max_tokens: 500
       }
     )
 
     do_response = client.completions(
       parameters: {
-        model: 'text-davinci-001',
-        prompt: do_prompt,
-        max_tokens: 300
+      model: 'text-davinci-003',
+      prompt: do_prompt,
+      max_tokens: 500
       }
     )
 
     explore_response = client.completions(
       parameters: {
-        model: 'text-davinci-001',
-        prompt: explore_prompt,
-        max_tokens: 300
+      model: 'text-davinci-003',
+      prompt: explore_prompt,
+      max_tokens: 300
       }
     )
 
-    @eat_response = eat_response["choices"][0]["text"]
+    eat_infos = JSON.parse(eat_response.dig('choices', 0, 'text'))
+    eat_infos.map { |info| Activity.create(name: info['name'], description: info['description'], category: 0, trip: @trip) }
+
+    do_infos = JSON.parse(do_response.dig('choices', 0, 'text'))
+    do_infos.map { |info| Activity.create(name: info['name'], description: info['description'], category: 1, trip: @trip) }
+
+    explore_infos = JSON.parse(explore_response.dig('choices', 0, 'text'))
+    explore_infos.map { |info| Activity.create(name: info['name'], description: info['description'], category: 2, trip: @trip) }
   end
 end
