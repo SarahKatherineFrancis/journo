761e797e8e1c05410f8709f47124200bd166ac78
Kirsty Brown <kirstyannbrown4@gmail.com>
2023-03-16 12:00:51 +0200

    feat:create a note

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,20 @@
 class NotesController < ApplicationController
+  def index
+  end
+
+  def new
+    @note = Note.new
+  end
+
+  def create
+    @note = Note.new(note_params)
+    @note.trip_id = params[:trip_id]
+    redirect_to root_path if @note.save
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:comment)
+  end
 end
