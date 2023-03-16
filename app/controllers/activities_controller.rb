3b283f17c973cc3671d1e814bc8024fa508c5f99
Sarah Francis <sarah.katherinefrancis@gmail.com>
2023-03-16 13:36:21 +0200

    moved generating activities to trip model

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -5,53 +5,6 @@ class ActivitiesController < ApplicationController
     @explore = @trip.activities.where(category: :explore, status: :pending)
     @do = @trip.activities.where(category: :do, status: :pending)
     @selected_activities = selected_activities
-    destination = @trip.destination
-    @user = current_user
-    eat_preference = @user.eat_preference_list
-    do_preference = @user.do_preference_list
-
-    client = OpenAI::Client.new
-    eat_prompt = "Can you suggest 3 restaurants that are #{eat_preference} in #{destination}?
-    Response must be in a JSON. JSON has an array with objects that has keys called name and description."
-    do_prompt = "Can you suggest 3 activities that are #{do_preference} in #{destination}?
-    Response must be in a JSON. JSON has an array with objects that has keys called name and description."
-    explore_prompt = "Can you suggest 3 activities top in #{destination}? Response must be in a JSON. JSON has an array
-    with objects that has keys called name and description."
-
-    eat_response = client.completions(
-      parameters: {
-        model: 'text-davinci-003',
-        prompt: eat_prompt,
-        temperature: 0.3,
-        max_tokens: 500
-      }
-    )
-
-    do_response = client.completions(
-      parameters: {
-      model: 'text-davinci-003',
-      prompt: do_prompt,
-      max_tokens: 500
-      }
-    )
-
-    explore_response = client.completions(
-      parameters: {
-      model: 'text-davinci-003',
-      prompt: explore_prompt,
-      max_tokens: 300
-      }
-    )
-
-    eat_infos = JSON.parse(eat_response.dig('choices', 0, 'text'))
-    eat_infos.map { |info| Activity.create(name: info['name'], description: info['description'], category: 0, trip: @trip) }
-
-    do_infos = JSON.parse(do_response.dig('choices', 0, 'text'))
-    do_infos.map { |info| Activity.create(name: info['name'], description: info['description'], category: 1, trip: @trip) }
-
-    explore_infos = JSON.parse(explore_response.dig('choices', 0, 'text'))
-    explore_infos.map { |info| Activity.create(name: info['name'], description: info['description'], category: 2, trip: @trip) }
-
   end
 
   def selected_activities
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,4 +3,112 @@ class Trip < ApplicationRecord
   has_many :activities, dependent: :destroy
 
   validates :trip_name, :destination, :start_date, :end_date, presence: true
+
+  after_commit :generate_activities, :only [:create]
+
+  def generate_activities
+    generate_eat_activities
+    generate_do_activities
+    generate_explore_activities
+  end
+
+  def generate_eat_activities
+    destination = @trip.destination
+    @user = current_user
+    eat_preference = @user.eat_preference_list
+
+    eat_prompt = "Can you give me a name of a restuarant that is #{eat_preference} in #{destination}?"
+
+    client = OpenAI::Client.new
+
+    response = client.chat(
+      parameters: {
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: eat_prompt }],
+        temperature: 0.7
+      }
+    )
+
+    eat_name = response.dig("choices", 0, "message", "content")
+
+    eat_description_prompt = "Can you give me a short description of the place?"
+
+    response = client.chat(
+      parameters: {
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: eat_prompt }, { role: "system", content: eat_name }, { role: "user", content: eat_description_prompt }],
+        temperature: 0.7
+      }
+    )
+
+    eat_description = response.dig("choices", 0, "message", "content")
+
+    Activity.create(name: eat_name, description: eat_description, category: :eat, trip: self)
+  end
+
+  def generate_do_activities
+    destination = @trip.destination
+    @user = current_user
+    do_preference = @user.do_preference_list
+
+    do_prompt = "Can you give me a name of an that is #{do_preference} in #{destination}?"
+
+    client = OpenAI::Client.new
+
+    response = client.chat(
+      parameters: {
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: do_prompt }],
+        temperature: 0.7
+      }
+    )
+
+    do_name = response.dig("choices", 0, "message", "content")
+
+    do_description_prompt = "Can you give me a short description of the place?"
+
+    response = client.chat(
+      parameters: {
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: do_prompt }, { role: "system", content: do_name }, { role: "user", content: do_description_prompt }],
+        temperature: 0.7
+      }
+    )
+
+    do_description = response.dig("choices", 0, "message", "content")
+
+    Activity.create(name: do_name, description: do_description, category: :do, trip: self)
+  end
+
+  def generate_explore_activities
+    destination = @trip.destination
+
+    explore_prompt = "Can you give me a name of the top 3 things to do in #{destination}?"
+
+    client = OpenAI::Client.new
+
+    response = client.chat(
+      parameters: {
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: explore_prompt }],
+        temperature: 0.7
+      }
+    )
+
+    explore_name = response.dig("choices", 0, "message", "content")
+
+    explore_description_prompt = "Can you give me a short description of the place?"
+
+    response = client.chat(
+      parameters: {
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: explore_prompt }, { role: "system", content: explore_name }, { role: "user", content: explore_description_prompt }],
+        temperature: 0.7
+      }
+    )
+
+    explore_description = response.dig("choices", 0, "message", "content")
+
+    Activity.create(name: explore_name, description: explore_description, category: :explore, trip: self)
+  end
 end
