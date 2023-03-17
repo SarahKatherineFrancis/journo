b3b5405d6df9b321ee1aa18360296c3f35bcd969
Oscar Welton <oscarwelton@gmail.com>
2023-03-17 12:33:29 +0200

    feat: basic search functionality of favourites

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,5 +1,11 @@
 class FavouritesController < ApplicationController
-  def index
-    @favourites = Activity.where(status: :favourite)
+
+    def index
+      if params[:query].present?
+        sql_query = "name ILIKE :query OR description ILIKE :query"
+        @favourites = Activity.where(status: :favourite).where(sql_query, query: "%#{params[:query]}%")
+      else
+        @favourites = Activity.where(status: :favourite)
+      end
+    end
   end
-end
