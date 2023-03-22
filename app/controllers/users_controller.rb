869c79833d56c093434c7161f45e7b913ccbd9e2
Oscar Welton <oscarwelton@gmail.com>
2023-03-22 19:57:11 +0200

    fix: commented out all mapbox

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,17 @@
 class UsersController < ApplicationController
   def index
     @trips = Trip.where(user_id: current_user.id)
-    #   @markers = @trips.geocoded.map do |trip|
-    #     {
-    #       lat: trip.latitude,
-    #       lng: trip.longitude,
-    #       info_window_html: render_to_string(partial: "/trips/info_window",
-    #                                          locals: {
-    #                                            trip:
-    #                                          }),
-    #       marker_html: render_to_string(partial: "/trips/marker", locals: { trip: })
-    #     }
-    #   end
+      # @markers = @trips.geocoded.map do |trip|
+      #   {
+      #     lat: trip.latitude,
+      #     lng: trip.longitude,
+      #     info_window_html: render_to_string(partial: "/trips/info_window",
+      #                                        locals: {
+      #                                          trip:
+      #                                        }),
+      #     marker_html: render_to_string(partial: "/trips/marker", locals: { trip: })
+      #   }
+      # end
   end
 
   def show
