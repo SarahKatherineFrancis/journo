a551b6614ac3dd29637add9510e15d62d9006ca6
Sarah Francis <sarah.katherinefrancis@gmail.com>
2023-03-22 12:19:46 +0200

    refactoring

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -20,4 +20,20 @@ class Activity < ApplicationRecord
   scope :pending_eat, -> { where(category: :eat, status: :pending) }
   scope :pending_do, -> { where(category: :do, status: :pending) }
   scope :pending_explore, -> { where(category: :explore, status: :pending) }
+  scope :selected, -> { where(status: :added) }
+
+  def self.generate_markers_json(activities)
+    ac = ActionController::Base.new()
+    activities.geocoded.map do |activity|
+      {
+        lat: activity.latitude,
+        lng: activity.longitude,
+        info_window_html: ac.render_to_string(partial: "/shared/info_window",
+                                           locals: {
+                                             activity:
+                                           }),
+        marker_html: ac.render_to_string(partial: "/shared/marker", locals: { activity: })
+      }
+    end.to_json
+  end
 end
