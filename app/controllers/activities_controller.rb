9baa7f00288b1a4c4afe6291d113a4b97619c3ab
Oscar Welton <oscarwelton@gmail.com>
2023-03-23 09:51:33 +0200

    feat: added basic calendar

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -7,23 +7,13 @@ class ActivitiesController < ApplicationController
     @selected_activities = selected_activities
 
     @activities = @trip.activities.all
-    # @markers = selected_activities.geocoded.map do |activity|
-    #   {
-    #     lat: activity.latitude,
-    #     lng: activity.longitude,
-    #     info_window_html: render_to_string(partial: "/activities/info_window",
-    #                                        locals: {
-    #                                          activity:
-    #                                        }),
-    #     marker_html: render_to_string(partial: "/activities/marker", locals: { activity: })
-    #   }
-    # end
+
   end
 
   def selected_activities
     @trip = Trip.find(params[:trip_id])
     @selected_activities = @trip.activities.where(status: :added)
-    # @markers = Activity.generate_markers_json(@trip.activities.selected)
+    @markers = Activity.generate_markers_json(@trip.activities.selected)
   end
 
   def added
