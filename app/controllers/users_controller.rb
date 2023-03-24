9a5d4722ac011bc06b6274e61ffb37b95f9f10af
Oscar Welton <oscarwelton@gmail.com>
2023-03-24 11:34:38 +0200

    fix: user dahsboard now only shows existing trips

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,6 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     @trip = Trip.all.where(id: current_user)
-    @trips = Trip.all
     @popular_destinations = @user.popular_destinations
   end
 end
